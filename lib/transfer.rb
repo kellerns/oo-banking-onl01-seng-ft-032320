dab552648c937fd08aaa4ef076f7496d208c3ca9
Nicholas Keller <nic.keller328@gmail.com>
2020-04-08 16:53:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
+  attr_accessor :sender, :receiver, :amount, :status, :count
 
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
     @status = "pending"
+    @count = 0
   end
 
   def valid?
@@ -17,11 +18,12 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid?
+    if self.valid? && self.count = 0
       self.receiver.balance += self.amount
       self.sender.balance -= self.amount
     end
     self.status = "complete"
+    self.count += 1
   end
 
   def reverse_transfer
