ccd0e5cc89aa350718ad7156395c4a4ab4dae8ca
Nicholas Keller <nic.keller328@gmail.com>
2020-04-08 16:28:06 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,27 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :status, :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(ammount)
+    @balance += ammount
+  end
+
+  def display_balance
+    @balance
+  end
+
+  def valid?(account)
+    if account.status == "open" && account.balance > 0
+      true
+    end
+  end
+
+
 
 end
